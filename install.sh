038967ee0caf5d80e50eafee66a9fb119286c4cd
Suli Solutions <143193054+sulishpkcom@users.noreply.github.com>
2023-09-23 16:38:10 +0200

    Create install.sh
    This Bash script is a versatile utility for setting up development environments on Ubuntu-based systems. It provides a user-friendly menu-driven interface that allows you to choose from various development environments or create a custom environment based on your preferences. The script automates the installation and configuration of essential software components, such as web servers, databases, version control, and more, making it easier to kickstart your development projects.

---
+++ install.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# Function to create a new user if it doesn't exist
+create_user() {
+    read -p "Enter a username for MySQL and phpMyAdmin: " username
+    if id "$username" &>/dev/null; then
+        echo "User '$username' already exists."
+    else
+        sudo adduser "$username"
+        sudo usermod -aG sudo "$username"
+        echo "User '$username' created."
+    fi
+}
+
+# Function to install and configure Node.js Development Environment
+install_nodejs_env() {
+    sudo apt-get update
+    sudo apt-get install -y nodejs npm git mysql-server ufw
+    sudo systemctl enable mysql
+    sudo systemctl start mysql
+    echo "Node.js Development Environment installed and configured."
+}
+
+# Function to install and configure Apache Development Environment
+install_apache_env() {
+    sudo apt-get update
+    sudo apt-get install -y apache2 git php php-mysql phpmyadmin mysql-server ufw
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
+    sudo systemctl enable mysql
+    sudo systemctl start mysql
+    echo "Apache Development Environment installed and configured."
+}
+
+# Function to allow the user to select apps for the Custom Development Environment
+install_custom_env() {
+    selected_apps=()
+    
+    PS3="Select the apps you want for the Custom Development Environment (press Enter after each selection, 'Done' to finish): "
+    options=("Node.js" "Apache" "Git" "PHP" "phpMyAdmin" "MySQL" "UFW" "Done")
+    
+    while true; do
+        select opt in "${options[@]}"; do
+            case $opt in
+                "Node.js")
+                    selected_apps+=("nodejs" "npm")
+                    ;;
+                "Apache")
+                    selected_apps+=("apache2" "php" "php-mysql" "phpmyadmin" "mysql-server")
+                    ;;
+                "Git")
+                    selected_apps+=("git")
+                    ;;
+                "PHP")
+                    selected_apps+=("php" "php-mysql")
+                    ;;
+                "phpMyAdmin")
+                    selected_apps+=("phpmyadmin")
+                    ;;
+                "MySQL")
+                    selected_apps+=("mysql-server")
+                    ;;
+                "UFW")
+                    selected_apps+=("ufw")
+                    ;;
+                "Done")
+                    break 2
+                    ;;
+                *)
+                    echo "Invalid selection."
+                    ;;
+            esac
+            break
+        done
+    done
+
+    # Install selected apps
+    sudo apt-get update
+    sudo apt-get install -y "${selected_apps[@]}"
+    sudo systemctl enable mysql
+    sudo systemctl start mysql
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
+    echo "Custom Development Environment installed and configured."
+}
+
+# Display a custom credit text
+display_credit_text() {
+    echo -e "\n----------------------------------"
+    echo "This script was built by Your Name"
+    echo "----------------------------------"
+    sleep 2s
+}
+
+# Main script
+display_credit_text
+
+create_user
+
+echo "Select a development environment:"
+select env in "Node.js Development Environment" "Apache Development Environment" "Custom Development Environment" "Quit"
+do
+    case $env in
+        "Node.js Development Environment")
+            install_nodejs_env
+            ;;
+        "Apache Development Environment")
+            install_apache_env
+            ;;
+        "Custom Development Environment")
+            install_custom_env
+            ;;
+        "Quit")
+            break
+            ;;
+        *)
+            echo "Invalid selection."
+            ;;
+    esac
+done
+
+echo "Development environment installation completed."
